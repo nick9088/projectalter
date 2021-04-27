883ea6a434db7acdb15e5e411b7475dd94f1f70a
thedoofmodern <45699480+thedoofmodern@users.noreply.github.com>
2021-04-27 16:45:02 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$('input:radio[name="4.3E"]').change(
+    function(){
+        if (this.checked && this.value == 'Yes') {
+           alert("Click OK To continue...");
+        }
+    });
+$('input:radio[name="4.3J"]').change(
+    function(){
+        if (this.checked && this.value == 'Yes') {
+           alert("Click OK To continue...");
+        }
+    });
+$('input:radio[name="4.3K"]').change(
+    function(){
+        if (this.checked && this.value == 'Yes') {
+           alert("Click OK To continue...");
+        }
+    });
+$('input:radio[name="4.3U"]').change(
+    function(){
+        if (this.checked && this.value == 'Yes') {
+           alert("Click OK To continue...");
+        }
+    });
+
+	
