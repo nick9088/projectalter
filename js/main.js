75613076dfea68f56391f45e340e78006fd45c02
thedoofmodern <45699480+thedoofmodern@users.noreply.github.com>
2021-04-27 16:47:55 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
-$('input:radio[name="4.3E"]').change(
-    function(){
-        if (this.checked && this.value == 'Yes') {
-           alert("Click OK To continue...");
-        }
-    });
-$('input:radio[name="4.3J"]').change(
-    function(){
-        if (this.checked && this.value == 'Yes') {
-           alert("Click OK To continue...");
-        }
-    });
-$('input:radio[name="4.3K"]').change(
-    function(){
-        if (this.checked && this.value == 'Yes') {
-           alert("Click OK To continue...");
-        }
-    });
-$('input:radio[name="4.3U"]').change(
-    function(){
-        if (this.checked && this.value == 'Yes') {
-           alert("Click OK To continue...");
-        }
-    });
+var radio_43E = document.getElementById('43E');
+var radio_43J = document.getElementById('43J');
+var radio_43K = document.getElementById('43K');
+var radio_43U = document.getElementById('43U');
+
+var button = document.getElementById('download');
+
+button.onclick = downloadFile;
+
+function downloadFile() {
+    if(radio_43E.checked) {
+        window.open("app/43E.app");
+    }else if(radio_43J.checked) {
+        window.open("app/43J.app");
+	}else if(radio_43U.checked) {
+        window.open("app/43U.app");
+	}else if(radio_43K.checked) {
+        window.open("app/43K.app");
+    } else {
+        alert("Please check one of the options first.");
+    }
+}
 
 	
